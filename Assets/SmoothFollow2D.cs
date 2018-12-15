677c9115f8186c67c4704c304d1736c8f948ebaf
Seyed Morteza Kamali <smkplus3d@gmail.com>
2018-12-15 15:04:47 +0330

    working on fart

---
+++ Assets/SmoothFollow2D.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow2D : MonoBehaviour
+{
+    public Vector2 minimumYPos;
+
+    //offset from the viewport center to fix damping
+    public float m_DampTime = 10f;
+    public Transform m_Target;
+    public float m_XOffset = 0;
+    public float m_YOffset = 0;
+
+    private float margin = 0.1f;
+
+    void Start()
+    {
+        if (m_Target == null)
+        {
+            m_Target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (m_Target)
+        {
+            float targetX = m_Target.position.x + m_XOffset;
+            float targetY = m_Target.position.y + m_YOffset;
+
+            if (Mathf.Abs(transform.position.x - targetX) > margin)
+                targetX = Mathf.Lerp(transform.position.x, targetX, 1 / m_DampTime * Time.deltaTime);
+
+            if (Mathf.Abs(transform.position.y - targetY) > margin)
+                targetY = Mathf.Lerp(transform.position.y, targetY, m_DampTime * Time.deltaTime);
+
+            transform.position = new Vector3(targetX, targetY, transform.position.z);
+        }
+        var pos = transform.position;
+        pos.y = Mathf.Clamp(pos.y,minimumYPos.x, minimumYPos.y);
+        transform.position = pos;
+    }
+}
